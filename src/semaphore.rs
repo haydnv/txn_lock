59f198d8cc08a4af1c19e06182717f36f4fe75e9
Haydn Vestal <haydnv@gmail.com>
2023-01-26 18:44:41 +0530

    implement Semaphore::read

--- src/semaphore.rs
+++ src/semaphore.rs
@@ -13,7 +13,9 @@ use std::sync::{Arc, Mutex};
 
 use tokio::sync::{Notify, OwnedSemaphorePermit};
 
-use super::{Error, Result};
+use super::Result;
+
+const PERMITS: u32 = u32::MAX >> 3;
 
 pub trait Overlap {
     fn overlaps(&self, other: &Self) -> bool;
@@ -51,40 +53,76 @@ struct Version<Range> {
 }
 
 impl<Range> Version<Range> {
-    fn new(permits: usize) -> Self {
+    fn new(reservations: Vec<Reservation<Range>>) -> Self {
         Self {
-            semaphore: Arc::new(tokio::sync::Semaphore::new(permits)),
-            reservations: Vec::new(),
+            semaphore: Arc::new(tokio::sync::Semaphore::new(PERMITS as usize)),
+            reservations,
         }
+    }
+
+    fn reserve(reservation: Reservation<Range>) -> Self {
+        Self::new(vec![reservation])
     }
 }
 
-enum ReadResult<TxnId> {
-    Version(Arc<tokio::sync::Semaphore>),
-    Pending(TxnId),
+enum VersionResult<TxnId, Range> {
+    Version(Arc<tokio::sync::Semaphore>, Arc<Range>),
+    Pending(TxnId, Arc<Range>),
 }
 
 /// A semaphore used to maintain the ACID compliance of transactional resource
 pub struct Semaphore<TxnId, Range> {
-    versions: Arc<Mutex<btree_map::BTreeMap<TxnId, Arc<Mutex<Version<Range>>>>>>,
+    versions: Arc<Mutex<btree_map::BTreeMap<TxnId, Version<Range>>>>,
     notify: Arc<Notify>,
 }
 
-impl<TxnId, Range: Overlap> Semaphore<TxnId, Range> {
-    fn read_inner(&self, txn_id: TxnId, range: &Arc<Range>) -> Result<ReadResult<TxnId>> {
-        todo!()
+impl<TxnId: Ord, Range: Overlap> Semaphore<TxnId, Range> {
+    /// Construct a new transactional [`Semaphore`].
+    pub fn new() -> Self {
+        Self {
+            versions: Arc::new(Mutex::new(btree_map::BTreeMap::new())),
+            notify: Arc::new(Notify::new()),
+        }
     }
 
-    /// Acquire a permit to read a transactional resource, if possible.
+    fn read_inner(&self, txn_id: TxnId, range: Arc<Range>) -> VersionResult<TxnId, Range> {
+        let mut versions = self.versions.lock().expect("versions");
+
+        if let Some(version) = versions.get(&txn_id) {
+            // this semaphore will take care of permits within this version
+            return VersionResult::Version(version.semaphore.clone(), range);
+        }
+
+        // handle creating a new version
+        for (_, version) in versions.iter().take_while(|(id, _)| *id < &txn_id) {
+            for reservation in &version.reservations {
+                if let Reservation::Write(locked) = reservation {
+                    if locked.overlaps(&range) {
+                        // if there's a write lock in the past, wait it out
+                        return VersionResult::Pending(txn_id, range);
+                    }
+                }
+            }
+        }
+
+        let version = Version::reserve(Reservation::Read(range.clone()));
+        let semaphore = version.semaphore.clone();
+        versions.insert(txn_id, version);
+
+        VersionResult::Version(semaphore, range)
+    }
+
+    /// Acquire a permit to read a section of transactional resource, if possible.
     pub async fn read(&self, mut txn_id: TxnId, range: Range) -> Result<Permit<Range>> {
-        let range = Arc::new(range);
+        let mut range = Arc::new(range);
 
         loop {
-            match self.read_inner(txn_id, &range)? {
-                ReadResult::Pending(id) => {
+            match self.read_inner(txn_id, range) {
+                VersionResult::Pending(id, r) => {
                     txn_id = id;
+                    range = r;
                 }
-                ReadResult::Version(semaphore) => {
+                VersionResult::Version(semaphore, range) => {
                     return Ok(Permit {
                         _permit: semaphore.acquire_owned().await?,
                         notify: self.notify.clone(),
