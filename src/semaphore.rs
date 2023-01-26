f04214107a74910ea4560709cd05c10a6b336473
Haydn Vestal <haydnv@gmail.com>
2023-01-26 19:33:28 +0530

    implement Semaphore::write

--- src/semaphore.rs
+++ src/semaphore.rs
@@ -7,13 +7,14 @@
 //!
 //! More information: [https://en.wikipedia.org/wiki/ACID](https://en.wikipedia.org/wiki/ACID)
 
+use std::cmp::Ordering;
 use std::collections::btree_map;
 use std::ops::Deref;
 use std::sync::{Arc, Mutex};
 
 use tokio::sync::{Notify, OwnedSemaphorePermit};
 
-use super::Result;
+use super::{Error, Result};
 
 const PERMITS: u32 = u32::MAX >> 3;
 
@@ -45,6 +46,15 @@ impl<Range> Drop for Permit<Range> {
 enum Reservation<Range> {
     Read(Arc<Range>),
     Write(Arc<Range>),
+}
+
+impl<Range> Reservation<Range> {
+    fn range(&self) -> &Range {
+        match self {
+            Self::Read(range) => range,
+            Self::Write(range) => range,
+        }
+    }
 }
 
 struct Version<Range> {
@@ -89,7 +99,7 @@ impl<TxnId: Ord, Range: Overlap> Semaphore<TxnId, Range> {
         let mut versions = self.versions.lock().expect("versions");
 
         if let Some(version) = versions.get(&txn_id) {
-            // this semaphore will take care of permits within this version
+            // this Tokio semaphore will coordinate access within this version
             return VersionResult::Version(version.semaphore.clone(), range);
         }
 
@@ -125,6 +135,74 @@ impl<TxnId: Ord, Range: Overlap> Semaphore<TxnId, Range> {
                 VersionResult::Version(semaphore, range) => {
                     return Ok(Permit {
                         _permit: semaphore.acquire_owned().await?,
+                        notify: self.notify.clone(),
+                        range,
+                    })
+                }
+            }
+
+            self.notify.notified().await;
+        }
+    }
+
+    fn write_inner(&self, txn_id: TxnId, range: Arc<Range>) -> Result<VersionResult<TxnId, Range>> {
+        let mut versions = self.versions.lock().expect("versions");
+
+        // handle creating a new version
+        for (version_id, version) in versions.iter() {
+            match txn_id.cmp(version_id) {
+                Ordering::Less => {
+                    for reservation in &version.reservations {
+                        if let Reservation::Write(locked) = reservation {
+                            if locked.overlaps(&range) {
+                                // if there's a write lock in the past, wait it out
+                                return Ok(VersionResult::Pending(txn_id, range));
+                            }
+                        }
+                    }
+                }
+                Ordering::Equal => {
+                    // the Tokio semaphore will coordinate access within this version
+                }
+                Ordering::Greater => {
+                    for reservation in &version.reservations {
+                        if reservation.range().overlaps(&range) {
+                            // can't allow writes to this range, there's already a future version
+                            return Err(Error::Conflict);
+                        }
+                    }
+                }
+            }
+        }
+
+        match versions.entry(txn_id) {
+            btree_map::Entry::Occupied(entry) => {
+                let version = entry.get();
+                Ok(VersionResult::Version(version.semaphore.clone(), range))
+            }
+            btree_map::Entry::Vacant(entry) => {
+                let version = Version::reserve(Reservation::Write(range.clone()));
+                let semaphore = version.semaphore.clone();
+                entry.insert(version);
+
+                Ok(VersionResult::Version(semaphore, range))
+            }
+        }
+    }
+
+    /// Acquire a permit to write to a section of transactional resource, if possible.
+    pub async fn write(&self, mut txn_id: TxnId, range: Range) -> Result<Permit<Range>> {
+        let mut range = Arc::new(range);
+
+        loop {
+            match self.write_inner(txn_id, range)? {
+                VersionResult::Pending(id, r) => {
+                    txn_id = id;
+                    range = r;
+                }
+                VersionResult::Version(semaphore, range) => {
+                    return Ok(Permit {
+                        _permit: semaphore.acquire_many_owned(PERMITS).await?,
                         notify: self.notify.clone(),
                         range,
                     })
