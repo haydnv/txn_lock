a2affb9f4c48c8795e37b566898bf91c44215e57
Haydn Vestal <haydnv@gmail.com>
2023-01-30 18:39:40 +0530

    implement TxnLock::{write, try_write}

--- src/scalar.rs
+++ src/scalar.rs
@@ -6,11 +6,12 @@
 //! use txn_lock::scalar::*;
 //! use txn_lock::Error;
 //!
-//! let lock = TxnLock::new(0, "zero");
+//! let lock = TxnLock::new("zero");
 //!
-//! assert_eq!(block_on(lock.read(0)).expect("read"), "zero");
-//! assert_eq!(*lock.try_read(0).expect("read"), "zero");
-//! // assert_eq!(lock.try_write(1).unwrap_err(), Error::WouldBlock);
+//! let value = block_on(lock.read(1)).expect("read");
+//! assert_eq!(*value, "zero");
+//! assert_eq!(lock.try_write(1).unwrap_err(), Error::WouldBlock);
+//! assert_eq!(*lock.try_read(1).expect("read"), "zero");
 //!
 //! {
 //!     // let commit = block_on(lock.commit(0)).expect("commit guard");
@@ -46,7 +47,6 @@
 use std::cmp::Ordering;
 use std::collections::BTreeMap;
 use std::fmt;
-use std::iter;
 use std::ops::Deref;
 use std::sync::{Arc, Mutex, MutexGuard};
 use std::task::Poll;
@@ -67,6 +67,7 @@ impl Overlaps<Range> for Range {
 }
 
 /// A read guard on a transactional value
+#[derive(Debug)]
 pub enum TxnLockReadGuard<T> {
     Committed(Arc<T>),
     Pending(Permit<Range>, OwnedRwLockReadGuard<T>),
@@ -83,51 +84,77 @@ impl<T> Deref for TxnLockReadGuard<T> {
     }
 }
 
-impl<T: PartialEq> PartialEq<T> for TxnLockReadGuard<T> {
+/// A read guard on a transactional value
+#[derive(Debug)]
+pub struct TxnLockWriteGuard<T> {
+    #[allow(unused)]
+    permit: Permit<Range>,
+    value: OwnedRwLockWriteGuard<T>,
+}
+
+impl<T> Deref for TxnLockWriteGuard<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        self.value.deref()
+    }
+}
+
+impl<T: PartialEq> PartialEq<T> for TxnLockWriteGuard<T> {
     fn eq(&self, other: &T) -> bool {
         self.deref().eq(other)
     }
 }
 
-impl<T: PartialOrd> PartialOrd<T> for TxnLockReadGuard<T> {
+impl<T: PartialOrd> PartialOrd<T> for TxnLockWriteGuard<T> {
     fn partial_cmp(&self, other: &T) -> Option<Ordering> {
         self.deref().partial_cmp(other)
     }
 }
 
-impl<T: fmt::Debug> fmt::Debug for TxnLockReadGuard<T> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        self.deref().fmt(f)
-    }
-}
-
-impl<T: fmt::Display> fmt::Display for TxnLockReadGuard<T> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        self.deref().fmt(f)
-    }
-}
-
 struct State<I, T> {
-    canon: Option<Arc<T>>,
+    canon: Arc<T>,
     committed: BTreeMap<I, Option<Arc<T>>>,
     pending: BTreeMap<I, Arc<RwLock<T>>>,
     finalized: Option<I>,
 }
 
-impl<I: Ord, T> State<I, T> {
-    fn new(txn_id: I, value: T) -> Self {
-        let version = Arc::new(RwLock::new(value));
-
+impl<I: Ord, T: Clone> State<I, T> {
+    fn new(canon: T) -> Self {
         State {
-            canon: None,
+            canon: Arc::new(canon),
             committed: BTreeMap::new(),
-            pending: BTreeMap::from_iter(iter::once((txn_id, version))),
+            pending: BTreeMap::new(),
             finalized: None,
         }
     }
 
     #[inline]
-    fn read_canon(&self, txn_id: &I) -> Result<Arc<T>> {
+    fn check_pending(&self, txn_id: &I) -> Result<()> {
+        if self.finalized.as_ref() >= Some(txn_id) {
+            Err(Error::Outdated)
+        } else if self.committed.contains_key(txn_id) {
+            Err(Error::Committed)
+        } else {
+            Ok(())
+        }
+    }
+
+    #[inline]
+    fn get_or_create_version(&mut self, txn_id: I) -> Arc<RwLock<T>> {
+        if let Some(version) = self.pending.get(&txn_id) {
+            version.clone()
+        } else {
+            let canon = self.read_canon(&txn_id);
+            let value = T::clone(&*canon);
+            let version = Arc::new(RwLock::new(value));
+            self.pending.insert(txn_id, version.clone());
+            version
+        }
+    }
+
+    #[inline]
+    fn read_canon(&self, txn_id: &I) -> &Arc<T> {
         let committed = self
             .committed
             .iter()
@@ -137,11 +164,11 @@ impl<I: Ord, T> State<I, T> {
 
         for version in committed {
             if let Some(version) = version {
-                return Ok(version.clone());
+                return version;
             }
         }
 
-        self.canon.clone().ok_or_else(|| Error::Outdated)
+        &self.canon
     }
 
     #[inline]
@@ -150,25 +177,41 @@ impl<I: Ord, T> State<I, T> {
             Poll::Ready(Err(Error::Outdated))
         } else if self.committed.contains_key(&txn_id) {
             assert!(!self.pending.contains_key(&txn_id));
-            Poll::Ready(self.read_canon(&txn_id))
+            Poll::Ready(Ok(self.read_canon(&txn_id).clone()))
         } else {
             Poll::Pending
         }
     }
 
     #[inline]
-    fn read_pending(&self, txn_id: &I, permit: Permit<Range>) -> Result<TxnLockReadGuard<T>> {
-        if let Some(version) = self.pending.get(&txn_id) {
-            // the permit means it's safe to call try_read_owned().expect()
-            let guard = version.clone().try_read_owned().expect("version");
-            return Ok(TxnLockReadGuard::Pending(permit, guard));
-        }
+    fn read_pending(&mut self, txn_id: I, permit: Permit<Range>) -> TxnLockReadGuard<T> {
+        // the permit means it's safe to call try_read_owned().expect()
+        let value = self
+            .get_or_create_version(txn_id)
+            .try_read_owned()
+            .expect("version");
 
-        self.read_canon(txn_id).map(TxnLockReadGuard::Committed)
+        return TxnLockReadGuard::Pending(permit, value);
+    }
+
+    #[inline]
+    fn write(&mut self, txn_id: I, permit: Permit<Range>) -> Result<TxnLockWriteGuard<T>> {
+        let value = self
+            .get_or_create_version(txn_id)
+            .try_write_owned()
+            .expect("version");
+
+        Ok(TxnLockWriteGuard { permit, value })
     }
 }
 
-/// A futures-aware read-write lock on a scalar value which supports transactional versioning.
+/// A futures-aware read-write lock on a scalar value which supports transactional versioning,
+/// where "scalar" means that the lock uses a single global range.
+///
+/// For non-scalar data types (e.g. a list, set, map, etc) consider using an alternate lock type,
+/// or implementing a custom lock type using [`Semaphore`].
+///
+/// The type `T` to lock must implement [`Clone`] in order to support versioning.
 pub struct TxnLock<I, T> {
     state: Arc<Mutex<State<I, T>>>,
     semaphore: Semaphore<I, Range>,
@@ -190,11 +233,11 @@ impl<I, T> TxnLock<I, T> {
     }
 }
 
-impl<I: Copy + Ord, T: fmt::Debug> TxnLock<I, T> {
+impl<I: Copy + Ord, T: Clone + fmt::Debug> TxnLock<I, T> {
     /// Construct a new [`TxnLock`].
-    pub fn new(txn_id: I, initial_value: T) -> Self {
+    pub fn new(canon: T) -> Self {
         Self {
-            state: Arc::new(Mutex::new(State::new(txn_id, initial_value))),
+            state: Arc::new(Mutex::new(State::new(canon))),
             semaphore: Semaphore::new(),
         }
     }
@@ -207,7 +250,7 @@ impl<I: Copy + Ord, T: fmt::Debug> TxnLock<I, T> {
         }
 
         let permit = self.semaphore.read(txn_id, Range).await?;
-        self.state().read_pending(&txn_id, permit)
+        Ok(self.state().read_pending(txn_id, permit))
     }
 
     /// Acquire a read lock for this value at `txn_id` synchronously, if possible.
@@ -218,6 +261,24 @@ impl<I: Copy + Ord, T: fmt::Debug> TxnLock<I, T> {
         }
 
         let permit = self.semaphore.try_read(txn_id, Range)?;
-        self.state().read_pending(&txn_id, permit)
+        Ok(self.state().read_pending(txn_id, permit))
+    }
+
+    /// Acquire a write lock for this value at `txn_id`.
+    pub async fn write(&self, txn_id: I) -> Result<TxnLockWriteGuard<T>> {
+        // before acquiring a permit, check if this version has already been committed
+        self.state().check_pending(&txn_id)?;
+
+        let permit = self.semaphore.write(txn_id, Range).await?;
+        self.state().write(txn_id, permit)
+    }
+
+    /// Acquire a write lock for this value at `txn_id` synchronously, if possible.
+    pub fn try_write(&self, txn_id: I) -> Result<TxnLockWriteGuard<T>> {
+        // before acquiring a permit, check if this version has already been committed
+        self.state().check_pending(&txn_id)?;
+
+        let permit = self.semaphore.try_write(txn_id, Range)?;
+        self.state().write(txn_id, permit)
     }
 }
